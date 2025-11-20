87ade5a743869c9ebd052f51b4703feef5afecc6
Muelvin Lopez <muelvinlopez25@gmail.com>
2025-11-20 16:48:37 +0800

    connected frontend and backend in login

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,19 +1,85 @@
 import "../components-css/auth.css"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+import api from "../axios"
 
 export default function Login({ email, setEmail, password, setPassword }) {
+
+    const navigate = useNavigate()
+
+    const handleEmail = (e) => {
+        setEmail(e.target.value)
+        console.log(e.target.value)
+    }
+
+    const handlePassword = (e) => {
+        setPassword(e.target.value)
+        console.log(e.target.value)
+    }
+
+    async function login(user) {
+        try {
+            const res = await api.post("/user/login", user)
+            return res.data
+        } catch (err) {
+            if (err.response) {
+                console.log(err.response.data.detail)
+            } else {
+                console.log(`Network or CORS error: ${err.message}`)
+            }
+            return null
+        }
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        const user = {
+            email: email,
+            password: password
+        }
+
+        const check_user = await login(user)
+
+        if (check_user) {
+            navigate("/dashboard")
+        } else {
+            alert("Invalid credentials")
+        }
+
+        setEmail("")
+        setPassword("")
+    }
+
     return (
         <>
             <div className="auth-container">
+
                 <h2>Sign in to your account</h2>
-                <form className="auth-form">
+
+                <form className="auth-form" onSubmit={handleSubmit}>
                     <p>Your email</p>
-                    <input type="text" />
+                    <input 
+                        type="text" 
+                        placeholder="example@gmail.com"
+                        value={email}
+                        onChange={handleEmail}
+                        required
+                    />
+
                     <p>Password</p>
-                    <input type="text" />
+                    <input 
+                        type="password" 
+                        placeholder="password1234"
+                        value={password}
+                        onChange={handlePassword}
+                        required
+                    />
+
                     <button className="auth-submit">Signin</button>
                 </form>
+
                 <p>Don't have an account yet? <Link to="/signup">Find here</Link></p>
+
             </div>
         </>
     )
